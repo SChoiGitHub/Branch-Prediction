89cb863dcdf28823546e25021416fab416217717
Sherman Choi <schoi@localhost.localdomain>
2017-06-02 15:19:13 -0500

    Added threading, but it looks bad since all of it is in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,51 @@
 #include <functional>
 #include <algorithm>
 #include <string>
+#include <thread>
 
 #include "parse/parse.h"
 #include "profile/profile.h"
 
+
+std::unordered_map<uint64_t,BBlock> blocks_in_file;
+
+
+void do_back_h(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).back_h(0);
+	}
+}
+
+void do_back_h_back_branches_only(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).back_h_back_branches_only(1);
+	}
+}
+void do_back_h_forward_branches_only(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).back_h_forward_branches_only(2);
+	}
+}
+void do_return_h(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).return_h(3,blocks_in_file);
+	}
+}
+void do_call_h(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).call_h(4,blocks_in_file);
+	}
+}
+void do_store_h(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).store_h(5,blocks_in_file);
+	}
+}
+void do_combined_h(){
+    for(auto& block : blocks_in_file){
+		std::get<1>(block).combined_h(6,blocks_in_file);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	
@@ -20,7 +61,7 @@ int main(int argc, char *argv[]) {
     }
     std::string path(argv[1]); 
     //I make a hash table. I associate the tags (address line of each block to the blocks themselves.
-    std::unordered_map<uint64_t,BBlock> blocks_in_file = Parse::parse_file(path);
+    blocks_in_file = Parse::parse_file(path);
     std::cout << "Completed parsing.\n";
     
 
@@ -33,20 +74,36 @@ int main(int argc, char *argv[]) {
 		std::get<1>(block).discoverParents(blocks_in_file);
 	}
     
+    std::thread back(do_back_h);
+    std::thread back_b(do_back_h_back_branches_only);
+    std::thread back_f(do_back_h_forward_branches_only);
+	std::thread return_h(do_return_h);
+    std::thread call_h(do_call_h);
+    std::thread combined_h(do_combined_h);
     
-    for(auto& block : blocks_in_file){
-		//Iterate between all blocks and test the heuristics.
-		//std::cout << std::get<1>(block).getFirstInstructionLocation() << '\n'; //A line of debugging.
-		std::get<1>(block).back_h(0);
-		std::get<1>(block).back_h_back_branches_only(1);
-		std::get<1>(block).back_h_forward_branches_only(2);
-		std::get<1>(block).return_h(3,blocks_in_file);
-		std::get<1>(block).call_h(4,blocks_in_file);
-		std::get<1>(block).store_h(5,blocks_in_file);
-		std::get<1>(block).combined_h(6,blocks_in_file);
+    if(back.joinable()){
+		back.join();
 	}
+	if(back_b.joinable()){
+		back_b.join();
+	}
+	if(back_f.joinable()){
+		back_f.join();
+	}
+    if(return_h.joinable()){
+		return_h.join();
+	}
+	if(call_h.joinable()){
+		call_h.join();
+	}
+	if(combined_h.joinable()){
+		combined_h.join();
+	}
+	
+	
 	std::cout << "Completed heuristic testing.\n";
 	BBlock::printHeuristicInformation();
 	
 	
 }
+
